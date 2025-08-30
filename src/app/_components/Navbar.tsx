6d06e8e48ec248cf8d7f1d494bd75087bc3d9831
crocodilelurker <swagatsahu556@gmail.com>
2025-08-31 02:22:25 +0530

    Added Navbar and Footer Components and Joined them in Root Layout

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -5,11 +5,14 @@ import { motion } from "framer-motion";
 
 export default function Navbar() {
   return (
+   
     <motion.nav 
       initial={{ y: -80, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.6, ease: "easeOut" }}
-      className="flex items-center justify-between px-10 py-4 bg-[#0a0f24]/90 backdrop-blur-md shadow-lg border-b border-blue-800 sticky top-0 z-50"
+      className="flex items-center justify-between px-10 py-4 
+bg-[#0a0f24]/90 bg-clip-padding backdrop-filter backdrop-blur-md 
+shadow-lg border-b border-gray-700 sticky top-0 z-50"
     >
       <div className="text-3xl font-extrabold bg-gradient-to-r from-cyan-400 via-blue-400 to-purple-400 bg-clip-text text-transparent animate-gradient">
         E Summit 2025
